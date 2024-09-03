e76d0461d6aa342e28886e8db2202e0df00d1c1d
connorwyx <connorwyx4@gmail.com>
2024-09-03 23:46:17 +0800

    made a new event listener for the adding block

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 const form = document.querySelector("form");
 const newInput = document.querySelector("input");
 const main = document.querySelector(".main");
+const week = [null, "MON", "TUE", "THU", "WED", "FRI", "SAT", "SUN"];
+let data = [];
+function alt(e) {
+  console.log(e);
+}
+function cancel(e) {
+  console.log(e);
+}
 
-console.log(form, newInput, main);
 form.addEventListener("submit", (e) => {
   console.log(e);
   e.preventDefault();
+  const todo = e.target[0].value;
+  const NewMemo = document.createElement("div");
+  const time = new Date();
+  const date = `${time.getFullYear()} ${time.getMonth()} ${time.getDate()} ${
+    week[time.getDay()]
+  }`;
+  NewMemo.id = time.getTime();
+  NewMemo.className = "memo";
+  NewMemo.innerHTML = `
+          <!-- left -->
+          <div class="left">
+            <div class="circle"></div>
+            <div class="box">
+              <p>${todo}</p>
+              <small>${date}</small>
+            </div>
+          </div>
+          <!-- right -->
+          <div class="right">
+            <span id="pen">✏️</span>
+            <span id="trash">🚫</span>
+          </div>`;
+  main.append(NewMemo);
+  const pen = NewMemo.lastElementChild.firstElementChild;
+  const trash = NewMemo.lastElementChild.lastElementChild;
+  pen.addEventListener("click", alt);
+  trash.addEventListener("click", cancel);
+  data.push({ id: time.getTime(), content: todo });
+  localStorage.setItem("data", JSON.stringify(data));
 });
